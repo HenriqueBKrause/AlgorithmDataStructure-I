85b91f1b124d7b24deac42bff111a9e056d1ecfe
HenriqueKrause <henriquebkrause2312@gmail.com>
2023-08-14 08:22:56 -0300

    :rocket: problem Leetcode

--- LeetCode/Exercicio7/src/main.c
+++ LeetCode/Exercicio7/src/main.c
@@ -1,8 +1,32 @@
+/*387. Primeiro caractere único em uma cadeia de caracteres
+*/
+
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
-    printf("Hello World!\n");
 
+int firstUniqChar(char * s){
+
+    int i, j, count = 0;
+    int len = strlen(s);
+    int flag = 0;
+    
+    for(i = 0; i < len; i++){
+        for(j = 0; j < len; j++){
+            if(s[i] == s[j]){
+                count++;
+            }
+        }
+        if(count == 1){
+            flag = 1;
+            return i;
+        }
+        count = 0;
+    }
+    if(flag == 0){
+        return -1;
+    }
     return 0;
+
+
+
 }
